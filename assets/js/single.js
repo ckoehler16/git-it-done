9b78e8ca00c50aee83c843758deb0904ede2e269
Koehler <ckoehler16@gmail.com>
2022-04-19 09:22:37 -0400

    linked index.html and single-repo.html and fixed possible user errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,14 +1,32 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
-var getRepoIssues = function(repo) {
+var getRepoName = function (name) {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
+
+var getRepoIssues = function (repo) {
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-    fetch(apiUrl).then(function(response) {
+    // make a get request to url
+    fetch(apiUrl).then(function (response) {
         // request was successful
         if (response.ok) {
-            response.json().then(function(data) {
+            response.json().then(function (data) {
                 // pass response data to dom function
                 displayIssues(data);
 
@@ -19,14 +37,13 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successfull, redirect to the homepage
+            document.location.replace("./index.html");
         }
     });
 };
 
-getRepoIssues("facebook/react");
-
-var displayIssues = function(issues) {
+var displayIssues = function (issues) {
     // checking for no issues
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
@@ -60,12 +77,12 @@ var displayIssues = function(issues) {
 
         // append to container
         issueEl.appendChild(typeEl);
-        
+
         issueContainerEl.appendChild(issueEl);
     }
 };
 
-var displayWarning = function(repo) {
+var displayWarning = function (repo) {
     // add text to warning container
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
@@ -76,4 +93,6 @@ var displayWarning = function(repo) {
 
     // append to warning container
     limitWarningEl.appendChild(linkEl);
-}
+};
+
+getRepoName();
